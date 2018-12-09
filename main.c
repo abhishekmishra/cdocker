c82d325607513c6b2012af34aa024239ae12a3b4
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-12-09 20:35:35 +0530

    added call to json and get id

--- main.c
+++ main.c
@@ -1,13 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <curl/curl.h>
+#include <json-c/json.h>
+
+struct MemoryStruct
+{
+    char *memory;
+    size_t size;
+};
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if(ptr == NULL)
+    {
+        /* out of memory! */
+        printf("not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
 
 int main()
 {
     CURL *curl;
     CURLcode res;
     struct curl_slist *headers = NULL;
+
+    struct MemoryStruct chunk;
+
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+    chunk.size = 0;    /* no data at this point */
+
+    json_object *new_obj;
 
     curl_global_init(CURL_GLOBAL_ALL);
 
@@ -28,17 +65,54 @@ int main()
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"Image\": \"alpine\", \"Cmd\": [\"echo\", \"hello world\"]}");
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1L);
 
+        /* send all data to this function  */
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+        /* we pass our 'chunk' struct to the callback function */
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+        /* some servers don't like requests that are made without a user-agent
+           field, so we provide one */
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
         /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
         /* Check for errors */
         if(res != CURLE_OK)
+        {
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                     curl_easy_strerror(res));
+        }
+        else
+        {
+            /*
+             * Now, our chunk.memory points to a memory block that is chunk.size
+             * bytes big and contains the remote file.
+             *
+             * Do something nice with it!
+             */
 
-        /* always cleanup */
-        curl_easy_cleanup(curl);
+            printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
+            printf("Data is [%s].\n", chunk.memory);
+            new_obj = json_tokener_parse(chunk.memory);
+            printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
+            json_object* idObj;
+            if (json_object_object_get_ex(new_obj, "Id", &idObj))
+            {
+                printf("Container Id = %s\n", json_object_to_json_string(idObj));
+            }
+            else
+            {
+                printf("Id not found.");
+            }
+
+            free(chunk.memory);
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+        }
+        curl_global_cleanup();
     }
-    curl_global_cleanup();
     return 0;
-    return 0;
+
 }

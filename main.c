91553bf8fc5625dbcbcec7bd15e721dadc5182c9
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-12-09 22:02:06 +0530

    refactored to some util functions to call api

--- main.c
+++ main.c
@@ -33,20 +33,14 @@ WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
     return realsize;
 }
 
-int main()
+int docker_api_post(char* url, char* post_data, struct MemoryStruct *chunk)
 {
     CURL *curl;
     CURLcode res;
     struct curl_slist *headers = NULL;
 
-    struct MemoryStruct chunk;
-
-    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
-    chunk.size = 0;    /* no data at this point */
-
-    json_object *new_obj;
-
-    curl_global_init(CURL_GLOBAL_ALL);
+    chunk->memory = malloc(1);  /* will be grown as needed by the realloc above */
+    chunk->size = 0;    /* no data at this point */
 
     /* get a curl handle */
     curl = curl_easy_init();
@@ -55,21 +49,21 @@ int main()
         /* First set the URL that is about to receive our POST. This URL can
            just as well be a https:// URL if that is what should receive the
            data. */
-        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.33:2376/containers/create");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
 
         headers = curl_slist_append(headers, "Expect:");
         headers = curl_slist_append(headers, "Content-Type: application/json");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
         /* Now specify the POST data */
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"Image\": \"alpine\", \"Cmd\": [\"echo\", \"hello world\"]}");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_data);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1L);
 
         /* send all data to this function  */
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 
         /* we pass our 'chunk' struct to the callback function */
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)chunk);
 
         /* some servers don't like requests that are made without a user-agent
            field, so we provide one */
@@ -92,27 +86,50 @@ int main()
              * Do something nice with it!
              */
 
-            printf("%lu bytes retrieved\n", (unsigned long)chunk.size);
-            printf("Data is [%s].\n", chunk.memory);
-            new_obj = json_tokener_parse(chunk.memory);
-            printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
-            json_object* idObj;
-            if (json_object_object_get_ex(new_obj, "Id", &idObj))
-            {
-                printf("Container Id = %s\n", json_object_to_json_string(idObj));
-            }
-            else
-            {
-                printf("Id not found.");
-            }
-
-            free(chunk.memory);
-
-            /* always cleanup */
-            curl_easy_cleanup(curl);
+            printf("%lu bytes retrieved\n", (unsigned long)chunk->size);
+            printf("Data is [%s].\n", chunk->memory);
         }
-        curl_global_cleanup();
+        /* always cleanup */
+        curl_easy_cleanup(curl);
     }
+    return 0;
+}
+
+char* docker_create_container() {
+    char* id;
+    json_object *new_obj;
+    struct MemoryStruct chunk;
+    docker_api_post("http://192.168.1.33:2376/containers/create", "{\"Image\": \"alpine\", \"Cmd\": [\"echo\", \"hello world\"]}", &chunk);
+
+    new_obj = json_tokener_parse(chunk.memory);
+    printf("new_obj.to_string()=%s\n", json_object_to_json_string(new_obj));
+    json_object* idObj;
+    if (json_object_object_get_ex(new_obj, "Id", &idObj))
+    {
+        char* container_id = json_object_to_json_string(idObj);
+        id = (char*)malloc((strlen(container_id) + 1) * sizeof(char));
+        strcpy(id, container_id);
+        printf("Container Id = %s\n", container_id);
+        printf("Container Id = %s\n", id);
+    }
+    else
+    {
+        printf("Id not found.");
+    }
+    free(chunk.memory);
+    return id;
+}
+
+int main()
+{
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    char* id;
+    id = docker_create_container();
+
+    printf("Docker container id is %s\n", id);
+
+    curl_global_cleanup();
     return 0;
 
 }

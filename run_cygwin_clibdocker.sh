f9b2e45e3296315cef994e8cc07a3ff06c6483c4
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-12-16 13:13:53 +0000

    fixed some vs build scripts

--- run_cygwin_clibdocker.sh
+++ run_cygwin_clibdocker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 . /cygdrive/c/Users/abhis/code/clibdocker/set_cygwin_env.sh
-/cygdrive/c/Users/abhis/code/clibdocker/clibdocker.exe
+if [ -z "$1" ]
+  then
+    echo "Running main program."
+	/cygdrive/c/Users/abhis/code/clibdocker/bin/debug/clibdocker.exe
+  else
+    echo "Running tests."
+    make test
+fi
+

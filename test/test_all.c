199ea4d48d0ded5325e6950126b2ba9507b1fdd1
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-12-17 19:19:03 +0530

    set log level to ignore debug messages in test

--- test/test_all.c
+++ test/test_all.c
@@ -11,8 +11,11 @@
 #include <cmocka.h>
 #include <stdio.h>
 #include "test_docker_containers.h"
+#include "log.h"
 
 int main(int argc, char **argv) {
+	log_set_level(LOG_INFO);
+	log_info("#### Starting clibdocker tests ####");
 	int res = docker_container_tests();
 	return res;
 }

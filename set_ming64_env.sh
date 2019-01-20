20d1974537aecbb3546d26e55cc60019b1e13126
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-01-20 17:51:04 +0530

    deleted unused files.

--- set_ming64_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export IUP_HOME=e:/IUP/iup-3.25_Win64_dllw6_lib
-export CD_HOME=e:/IUP/cd-5.11.1_Win64_dllw6_lib
-export IM_HOME=e:/IUP/im-3.12_Win64_dllw6_lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${IUP_HOME}:${CD_HOME}:${IM_HOME}
-export PATH=${PATH}:${IUP_HOME}:${CD_HOME}:${IM_HOME}
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/usr/lib/pkgconfig:/ming64/lib/pkgconfig:/ming64/share/lib/pkgconfig

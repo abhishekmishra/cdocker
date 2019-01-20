51ce9a9013142aed8b76de7837a5c71d49fbc193
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-01-20 22:14:07 +0530

    started simple cli

---
+++ src/docker_all.h
@@ -0,0 +1,35 @@
+/*
+* clibdocker: docker_all.h
+* Created on: 20-Jan-2019
+* 
+* clibdocker
+* Copyright (C) 2018 Abhishek Mishra <abhishekmishra3@gmail.com>
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SRC_DOCKER_ALL_H_
+#define SRC_DOCKER_ALL_H_
+
+#include "docker_util.h"
+#include "docker_result.h"
+#include "docker_connection_util.h"
+#include "docker_containers.h"
+#include "docker_images.h"
+#include "docker_networks.h"
+#include "docker_volumes.h"
+#include "docker_system.h"
+#include "docker_log.h"
+
+#endif /* SRC_DOCKER_ALL_H_ */

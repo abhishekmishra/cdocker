af134a439207bac7007984dbefb7319b6507cba2
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-10-10 19:32:54 +0530

    removed cld files and moved public headers to their own dir

--- src/cld_ctr.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_CLD_CTR_H_
-#define SRC_CLD_CTR_H_
-
-#include "cld_common.h"
-#include "cld_command.h"
-
-cld_command* ctr_commands();
-
-#endif
--- src/cld_img.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_CLD_IMG_H_
-#define SRC_CLD_IMG_H_
-
-#include "cld_common.h"
-#include "cld_command.h"
-
-cld_command* img_commands();
-
-#endif

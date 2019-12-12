0f179955d76280cffa1600fa8bf8aa3a74394628
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-12-12 17:03:07 +0530

    added container methods

--- include/lua_docker.h
+++ include/lua_docker.h
@@ -24,7 +24,24 @@ LUALIB_API int DockerClient_connect_url(lua_State* L);
 // DockerClient:new()
 LUALIB_API int DockerClient_new(lua_State* L);
 
+// container APIs
 LUALIB_API int DockerClient_container_list(lua_State* L);
+LUALIB_API int DockerClient_create_container(lua_State* L);
+LUALIB_API int DockerClient_inspect_container(lua_State* L);
+LUALIB_API int DockerClient_process_list_container(lua_State* L);
+LUALIB_API int DockerClient_container_logs(lua_State* L);
+LUALIB_API int DockerClient_container_changes(lua_State* L);
+LUALIB_API int DockerClient_container_stats(lua_State* L);
+LUALIB_API int DockerClient_start_container(lua_State* L);
+LUALIB_API int DockerClient_stop_container(lua_State* L);
+LUALIB_API int DockerClient_restart_container(lua_State* L);
+LUALIB_API int DockerClient_kill_container(lua_State* L);
+LUALIB_API int DockerClient_rename_container(lua_State* L);
+LUALIB_API int DockerClient_pause_container(lua_State* L);
+LUALIB_API int DockerClient_unpause_container(lua_State* L);
+LUALIB_API int DockerClient_wait_container(lua_State* L);
+LUALIB_API int DockerClient_remove_container(lua_State* L);
+
 
 LUALIB_API int luaopen_luaclibdocker(lua_State* L);
 

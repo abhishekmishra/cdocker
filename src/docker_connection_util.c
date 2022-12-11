d4dbf6c132d15365f7837bdc409466fc567a38b3
Abhishek Mishra <abhishekmishra3@gmail.com>
2022-12-11 12:18:45 +0530

    set proper error code on curl error

--- src/docker_connection_util.c
+++ src/docker_connection_util.c
@@ -5,18 +5,18 @@
  * This file is part of clibdocker.
  *
  * clibdocker is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as 
- * published by the Free Software Foundation, 
- * either version 3 of the License, or (at your option) 
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option)
  * any later version.
  *
- * clibdocker is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty 
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * clibdocker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
- * License along with clibdocker. 
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with clibdocker.
  * If not, see <https://www.gnu.org/licenses/>.
  *
  */
@@ -1065,6 +1065,7 @@ d_err_t docker_call_exec(docker_context *ctx, docker_call *dcall, json_object **
 				fprintf(stderr, "curl_easy_perform() failed: %s\n",
 						curl_easy_strerror(res));
 				result->error_code = E_CONNECTION_FAILED;
+				err = result->error_code;
 			}
 			else
 			{

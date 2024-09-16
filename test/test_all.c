05ac3f3ff63d7aefdc624f02382eb5c59124981a
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-16 23:27:57 +0530

    uncommented all tests

--- test/test_all.c
+++ test/test_all.c
@@ -42,41 +42,41 @@ int main(int argc, char **argv) {
 	docker_log_set_level(LOG_INFO);
 	docker_log_info("#### Starting clibdocker tests ####");
 
-	// docker_log_info("#### Docker container API      ####");
-	// res = docker_container_tests();
-	// docker_log_info("#### Done                      ####");
+	docker_log_info("#### Docker container API      ####");
+	res = docker_container_tests();
+	docker_log_info("#### Done                      ####");
 
-	// if (res > 0) {
-	// 	return res;
-	// }
+	if (res > 0) {
+		return res;
+	}
 
-	//docker_log_info("#### Docker images API      	####");
-	//res = docker_images_tests();
-	//docker_log_info("#### Done                      ####");
+	docker_log_info("#### Docker images API      	####");
+	res = docker_images_tests();
+	docker_log_info("#### Done                      ####");
 
-	//if (res > 0) {
-	//	return res;
-	//}
+	if (res > 0) {
+		return res;
+	}
 
-	//docker_log_info("#### Docker system API      	####");
-	//res = docker_system_tests();
-	//docker_log_info("#### Done                      ####");
+	docker_log_info("#### Docker system API      	####");
+	res = docker_system_tests();
+	docker_log_info("#### Done                      ####");
 
-	//if (res > 0) {
-	//	return res;
-	//}
+	if (res > 0) {
+		return res;
+	}
 
-	//docker_log_info("#### Docker networks API      	####");
-	//res = docker_networks_tests();
-	//docker_log_info("#### Done                      ####");
+	docker_log_info("#### Docker networks API      	####");
+	res = docker_networks_tests();
+	docker_log_info("#### Done                      ####");
 
-	//if (res > 0) {
-	//	return res;
-	//}
+	if (res > 0) {
+		return res;
+	}
 
-	//docker_log_info("#### Docker volumes API      	####");
-	//res = docker_volumes_tests();
-	//docker_log_info("#### Done                      ####");
+	docker_log_info("#### Docker volumes API      	####");
+	res = docker_volumes_tests();
+	docker_log_info("#### Done                      ####");
 
 	docker_log_info("#### Docker ignore files test  ####");
 	res = docker_ignore_tests();
